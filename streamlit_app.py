389e13a952ce1329a0f710057ed69cf6f56d5e04
Faisal Rahman <f.rahman.uob@gmail.com>
2023-03-27 02:04:03 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,24 @@
 import streamlit as st
 import pandas as pd
 import requests
+import matplotlib.pyplot as plt
 
-# Define the API endpoints for Binance
-BNB_URL = "https://api.binance.com/api/v3/klines?symbol=BNBUSDT&interval=15m&limit=50"
-BTC_URL = "https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=15m&limit=50"
+# Define the API endpoint for air quality data
+AQ_URL = "https://api.openaq.org/v1/latest?country=BD"
 
-# Fetch data from the API
-def get_data(url):
-    data = requests.get(url).json()
-    df = pd.DataFrame(data)
-    df.columns = ['open_time', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_asset_volume',
-                  'number_of_trades', 'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume', 'ignore']
-    df['open_time'] = pd.to_datetime(df['open_time'], unit='ms')
-    df['close_time'] = pd.to_datetime(df['close_time'], unit='ms')
-    return df
+# Fetch air quality data from the API
+data = requests.get(AQ_URL).json()
+results = data['results']
 
-# Get Binance data
-bnb_df = get_data(BNB_URL)
-btc_df = get_data(BTC_URL)
+# Convert data to Pandas DataFrame
+aq_df = pd.json_normalize(results)
+coordinates = pd.json_normalize(aq_df['coordinates'])
+aq_df = pd.concat([aq_df, coordinates], axis=1)
 
 # Plot the data
-st.line_chart(bnb_df[['open_time', 'close']].set_index('open_time'))
-st.line_chart(btc_df[['open_time', 'close']].set_index('open_time'))
+fig, ax = plt.subplots()
+ax.scatter(aq_df['longitude'], aq_df['latitude'], c=aq_df['measurements.value'], cmap='Reds')
+ax.set_xlabel('Longitude')
+ax.set_ylabel('Latitude')
+ax.set_title('Air Quality in Bangladesh')
+st.pyplot(fig)
